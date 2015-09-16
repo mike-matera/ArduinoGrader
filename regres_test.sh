580cb1efc84f413bef17e2ab0b857d3de92d86be
Mike Matera <matera@lifealgorithmic.com>
2015-09-15 17:36:27 -0700

    Added simple regression test script to test future API changes

---
+++ regres_test.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# This script does regression testing
+export PATH=/opt/arduino-1.6.5:$PATH
+
+set -e
+
+./run_test.sh sketches/WaitForHigh/WaitForHigh.ino tests/wait_for_high.cpp 
+./run_test.sh sketches/Blink/Blink.ino tests/blink_test.cpp
+./run_test.sh sketches/ColorView/ColorView.ino tests/color_view_test.cpp
