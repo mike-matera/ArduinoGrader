2016f52c4fff326fbeec01d603140d3013a4b368
Mike Matera <matera@lifealgorithmic.com>
2015-09-25 09:21:59 -0700

    Improvements to the grade script runner

--- grade.sh
+++ grade.sh
@@ -27,13 +27,21 @@ if [ ! -f "$1" ]; then
 fi
 
 # fixme:
-installdir=/home/mimatera/ArduinoGrader
+installdir=/home/maximus/Arduino/ArduinoGrader
 
-sketchbase=$(basename "$1")
-sketchdir=$(basename "$1" .ino)
-testbase=${sketchdir,,}_test.cpp
+sketchfix=$(echo "$1" | perl -pe 's/\s+\(\d+\)//');
+sketchfix=$(echo "$sketchfix" | perl -pe 's/\.ino\.ino/.ino/'); 
+echo fixed sketch name: $sketchfix
+sketchbase=$(basename "$sketchfix")
+sketchdir=$(basename "$sketchfix" .ino)
 
-if [ ! -f $installdir/tests/$testbase ]; then
+testbase="${sketchdir,,}"
+testbase=$(echo "$testbase" | perl -pe 's/\s+\(\d+\)//');
+testbase="${testbase,,}_test.cpp"
+echo test: "$testbase"
+
+
+if [ ! -f "$installdir/tests/$testbase" ]; then
     echo "No test for this file: $testbase"
     read DUMMY
     exit 3
@@ -47,31 +55,31 @@ else
     ncpus=2
 fi
 
-gedit "$1" & 
+gedit "$1" > /dev/null 2>&1 & 
 
-mkdir -p $tempdir/$sketchdir/build
+mkdir -p "$tempdir/$sketchdir/build"
 (
     set -e
-    echo $tempdir
-    cp "$1" $tempdir/$sketchdir
+    echo "$tempdir"
+    cp "$1" "$tempdir/$sketchdir/$(basename "$sketchfix")"
 
     # fix arduino on Cygwin
     if [ -n "$WINDIR" ]; then
-        tempsketch=$(cygpath -w -a $tempdir/$sketchdir/$sketchbase)
-        tempbuild=$(cygpath -w -a $tempdir/build)
+        tempsketch=$(cygpath -w -a "$tempdir/$sketchdir/$sketchbase")
+        tempbuild=$(cygpath -w -a "$tempdir/build")
     else
-        tempsketch=$tempdir/$sketchdir/$sketchbase
-        tempbuild=$tempdir/build
+        tempsketch="$tempdir/$sketchdir/$sketchbase"
+        tempbuild="$tempdir/build"
     fi
-    "$arduino" --verify --preserve-temp-files --pref build.path=$tempbuild $tempsketch 
-    cp $tempdir/build/${sketchdir}.cpp $tempdir
-    cp $installdir/emulator.make $installdir/emulator/* $installdir/arduino/* $tempdir
-    cp $installdir/tests/$testbase $tempdir 
-    make -j $ncpus -f emulator.make -C $tempdir all 
-    cd $tempdir
+    "$arduino" --verify --preserve-temp-files --pref build.path="$tempbuild" "$tempsketch"
+    cp "$tempdir/build/${sketchdir}.cpp" "$tempdir"
+    cp "$installdir"/emulator.make "$installdir"/emulator/* "$installdir"/arduino/* "$tempdir"
+    cp "$installdir"/tests/"$testbase" "$tempdir"
+    make -j $ncpus -f emulator.make -C "$tempdir" all 
+    cd "$tempdir"
     ./test
 )
-rm -rf $tempdir
+rm -rf "$tempdir"
 
 echo "Hit enter to continue..."
 read DUMMY
