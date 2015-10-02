956e41503837bb989ffc2e9de217961626849e0e
Mike Matera <matera@lifealgorithmic.com>
2015-10-01 17:35:26 -0700

    Adding test for voltage level.

---
+++ tests/volt_level_test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "emulator.h"
+
+using std::cout; 
+using std::endl;
+using std::string; 
+
+volatile int codes[] = {450, 451, 552, 553};
+volatile int voltage = -1;
+
+int test_getvalue(int pin, const PinState &state) {  
+  return codes[voltage];
+}
+
+bool test_loop(int count) {
+  voltage++;
+  return (count < 4);
+}
+
+void test_exit() {
+  cout << "Test finished, output should be: Low, Inter, Inter, High" << endl;
+}
