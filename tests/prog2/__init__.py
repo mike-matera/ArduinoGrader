0c75894ee02279aa0a18b88236218742d805e975
Mike Matera <matera@lifealgorithmic.com>
2016-09-21 08:07:29 -0700

    Native code wrapper can take arguments, use them

--- tests/prog2/__init__.py
+++ tests/prog2/__init__.py
@@ -4,7 +4,7 @@ import sys
 
 def do_part1(exe) :    
     try:
-        test = pexpect.spawnu(' '.join([exe, __name__ + ".prog2_1"]))
+        test = pexpect.spawnu(' '.join([exe, __name__ + ".prog2", 'part1']))
         test.logfile = sys.stdout
         print ("+ Testing if your sketch responds to serial input.")
         test.expect('.*period: (\d+)')
@@ -28,18 +28,20 @@ def do_part2(exe) :
     print ("+ Testing if part2 meets the part1 specification")
     do_part1(exe)
     try:
-        test = pexpect.spawnu(' '.join([exe, __name__ + ".prog2_2"]))
+        test = pexpect.spawnu(' '.join([exe, __name__ + ".prog2", 'part2r']))
         test.logfile = sys.stdout
         print ("+ Testing the left button.")
-        test.expect('.*period: 40\d')
-        print ("+ pass: got to 400")
-        test.expect('.*period: 30\d')
-        print ("+ pass: got to 300")
-        test.expect('.*period: 20\d')
-        print ("+ pass: got to 200")
+        test.expect('.*period: 100\d', timeout=120)
+        test.expect('.*period: 100\d')
+        print ("+ pass: got to 1000")
+        test.terminate()
+
+        test = pexpect.spawnu(' '.join([exe, __name__ + ".prog2", 'part2l']))
+        test.logfile = sys.stdout
+        print ("+ Testing the right button.")
+        test.expect('.*period: 10\d', timeout=120)
         test.expect('.*period: 10\d')
-        test.expect('.*period: 10\d')
-        print ("+ pass: got to 100")
+        print ("+ pass: got to 1000")
         test.terminate()
 
     except Exception as e: 
---
+++ tests/prog2/prog2.py
@@ -0,0 +1,61 @@
+import sketch
+from tests.fcounter import FrequencyCounter
+from tests.button import Button
+from emu.emulator import emu, Pin, PinMode
+
+ledpin = 2
+leftButton = 13
+rightButton = 12
+
+emu.pins[ledpin] = FrequencyCounter()
+emu.pins[leftButton] = Button()
+emu.pins[rightButton] = Button()
+
+def test_run(part) :
+    if part == 'part1' :
+        return test_run_part1()
+    elif part == 'part2l' :
+        return test_run_part2l()
+    elif part == 'part2r' :
+        return test_run_part2r()
+    else :
+        print ("ERROR: No test chosen.")
+        return False
+
+# part 1
+def test_run_part1() :    
+    sketch.setup();
+    while True :
+        sketch.loop();
+        counter = emu.pins[ledpin]
+        period = counter.get_period() / 2
+        if period > 0 :
+            print ("period:", int(period))
+
+# part 2
+def test_run_part2l() :    
+    sketch.setup();
+    for i in range(100) :
+        sketch.loop();
+        if i % 2  == 0 :
+            emu.pins[leftButton].push_for(0.1)
+
+        counter = emu.pins[ledpin]
+        period = counter.get_period() / 2
+        if period > 0 :
+            print ("period:", int(period))
+
+# part 2
+def test_run_part2r() :    
+    sketch.setup();
+    for i in range(100) :
+        sketch.loop();
+        if i % 2  == 0 :
+            emu.pins[rightButton].push_for(0.1)
+        
+        counter = emu.pins[ledpin]
+        period = counter.get_period() / 2
+        if period > 0 :
+            print ("period:", int(period))
+
+
