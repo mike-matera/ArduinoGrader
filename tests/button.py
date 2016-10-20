5837e5beef068da99cc048dc71096a7cb24e9099
Mike Matera <matera@lifealgorithmic.com>
2016-10-20 11:56:18 -0700

    Can trigger button in the future.

--- tests/button.py
+++ tests/button.py
@@ -4,17 +4,23 @@ from emu.emulator import Pin, PinMode
 class Button(Pin) :
     def __init__(self) :
         super().__init__()
-        self.armed = False;
+        self.armed = 0;
         
     def get_value(self, ts) :
         if self.mode == PinMode.kPullup : 
-            if self.armed :
-                self.armed = False
-                return 0
+            if self.armed > 0 :
+                self.armed -= 1
+                if self.armed == 0 :
+                    return 0
+                else:
+                    return 1
             else : 
                 return 1
         else:
             return self.value
 
     def push(self) :
-        self.armed = True
+        self.armed = 1
+
+    def push_delay(self, delay) :
+        self.armed = delay
