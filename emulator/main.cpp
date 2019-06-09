3cb045b7c63448956e5f088649d537c39ce3e852
Mike Matera <matera@lifealgorithmic.com>
2019-06-08 23:00:23 -0700

    Build pros during test build.

--- emulator/main.cpp
+++ emulator/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+#include <grpc/grpc.h>
+#include <grpc++/channel.h>
+#include <grpc++/client_context.h>
+#include <grpc++/create_channel.h>
+#include <grpc++/security/credentials.h>
+
+#include "proto/emulator.grpc.pb.h"
+
 int main(int argc, char **argv)
 {
     std::cout << "Hello World" << std::endl;
+
+    grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
+
     return 0;
 }
